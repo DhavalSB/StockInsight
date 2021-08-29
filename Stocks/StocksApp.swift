b80c8a9f9e69f5741cfe9d227b893c7073186dc1
Dhaval B <pythondsb@gmail.com>
2021-08-29 14:54:01 -0700

    Initial Commit

---
+++ Stocks/StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksApp.swift
+//  Stocks
+//
+//  Created by Dhaval Biradar on 8/29/21.
+//
+
+import SwiftUI
+
+@main
+struct StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
