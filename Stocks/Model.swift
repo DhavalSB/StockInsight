9175163723bd88cbe67650e4d4519460d644b5d2
Dhaval B <pythondsb@gmail.com>
2022-01-23 22:39:12 -0800

    Fixed getSector() Parsing Issue

--- Stocks/Model.swift
+++ Stocks/Model.swift
@@ -54,37 +54,46 @@ func isCS(stock: String) -> Bool {
 // RETURN N/A for Industry if ETF or not Common Stock
 func getSector(stock: String) -> String {
     var sector = ""
-do {
-    let originalURL = "https://finance.yahoo.com/quote/\(stock)/profile?p=\(stock)"
-    let replacedURL = originalURL.replacingOccurrences(of: " ", with: "")
-    let content = try String(contentsOf: URL(string: replacedURL)!)
-    let doc: Document = try SwiftSoup.parse(content)
-//   Check if html tag is Sector
-    if try (try doc.select("span[data-reactid='19']").indices.contains(0)) && (try doc.select("span[data-reactid='19']")[0].text() == "Sector(s)") {
-        let retrievedSector = try doc.select("span[data-reactid='21']")[0]
-        sector = try retrievedSector.text()
-    } else if try (try doc.select("span[data-reactid='21']").indices.contains(0)) && (try doc.select("span[data-reactid='21']")[0].text() == "Sector(s)") {
-         let retrievedSector = try doc.select("span[data-reactid='23']")[0]
-        sector = try retrievedSector.text()
-// Check if html tag is Legal Type (For ETF's)
-    } else if try (try doc.select("span[data-reactid='53']").indices.contains(0)) && (try doc.select("span[data-reactid='53']")[0].text() == "Legal Type"){
-        let type = try doc.select("span[data-reactid='54']")[0]
-        sector = try type.text()
-//  If Neither, Just return N/A
-    } else {
-        return "N/A"
-    }
-
-//    Error Handling
+    do {
+        let originalURL = "https://finance.yahoo.com/quote/\(stock)/profile?p=\(stock)"
+        let replacedURL = originalURL.replacingOccurrences(of: " ", with: "")
+        let content = try String(contentsOf: URL(string: replacedURL)!)
+        let doc: Document = try SwiftSoup.parse(content)
+        
+        //   Check if html tag is Sector
+        
+        if try (try doc.select("p[class='D(ib) Va(t)'] > span").indices.contains(0)) && (try doc.select("p[class='D(ib) Va(t)'] > span")[0].text() == "Sector(s)") {
+            print(try doc.select("p[class='D(ib) Va(t)'] > span")[1].text())
+            let retrievedSector = try doc.select("p[class='D(ib) Va(t)'] > span")[1]
+            sector = try retrievedSector.text()
+            
+        } else if try (try doc.select("span[data-reactid='21']").indices.contains(0)) && (try doc.select("span[data-reactid='21']")[0].text() == "Sector(s)") {
+            let retrievedSector = try doc.select("span[data-reactid='23']")[0]
+            sector = try retrievedSector.text()
+            
+            // Check if html tag is Legal Type (For ETF's)
+            
+            
+        } else if try (try doc.select("span[class='Mend(5px) Whs(nw)'] > span").indices.contains(5)) && (try doc.select("span[class='Mend(5px) Whs(nw)'] > span")[5].text() == "Legal Type"){
+            let type = try doc.select("span[class='Fl(end)']")[4]
+            sector = try type.text()
+            //  If Neither, Just return N/A
+            
+        } else {
+            return "N/A"
+        }
+        
+        //    Error Handling
     } catch Exception.Error(_, let message) {
-    print(message)
+        print(message)
     } catch {
         print("error")
     }
-
-   return sector
     
+    return sector
+
 }
+
 
 func getIndustry(stock: String) -> String {
     var industry = ""
